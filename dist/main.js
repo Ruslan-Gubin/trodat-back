abffa748b2e5dfb52bfe62191ff0929a9b4af625
ruslan1986 <gubin_ruslan@rambler.ru>
2024-07-05 11:30:35 +0300

    commit

--- dist/main.js
+++ dist/main.js
@@ -4,9 +4,13 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 const configs_1 = require("./modules/config/configs");
+const cors_1 = require("cors");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
+    app.use((0, cors_1.default)({
+        credentials: true,
+        origin: '*'
+    }));
     const appConfig = app.get(configs_1.AppConfig);
     app.useGlobalPipes(new common_1.ValidationPipe({
         transform: true,
